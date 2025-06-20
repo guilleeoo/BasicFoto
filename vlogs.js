930b5cbb9f7d37468f562796132a93476fd0b681
Nico Minguez <nico.minguez@bellevuecollege.edu>
2025-06-20 15:11:44 +0200

    initial commit

---
+++ vlogs.js
@@ -0,0 +1,22 @@
+const vlogs = [
+    {
+      id: "ciudad",
+      title: "Ciudad",
+      images: ["src/images/paloma.jpg", "src/images/plaza.jpg"],
+    },
+    {
+      id: "etxauri",
+      title: "Etxauri",
+      images: ["src/images/javiuso.jpg", "src/images/muro.jpg"],
+    },
+    {
+      id: "picos",
+      title: "Picos",
+      images: ["src/images/panti.jpg", "src/arcalis.jpg"],
+    },
+    {
+       id: "otro",
+       title: "Otro",
+       images: "src/images/sierra.jpg",
+    }
+  ];
